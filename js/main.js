2138c2d12308aafe9ced65dd7580fcbd30467628
fwchen <crystal.tyan@gmail.com>
2017-04-15 23:58:56 +0800

    initial commit

--- js/main.js
+++ js/main.js
@@ -4,6 +4,56 @@ document.addEventListener("WeixinJSBridgeReady", function () {
   shakeMusic.play();
 }, false);
 
+document.getElementById('button1').addEventListener('click', function() {
+  document.getElementById('page1').style.display = 'none';
+  document.getElementById('page2').style.display = 'block';
+});
+
+window.onload = function() {
+  var currentNumber = 1;
+  document.getElementById('number-add').addEventListener('click', function(event) {
+    currentNumber++;
+    document.getElementById('number').innerHTML = currentNumber;
+  });
+
+  document.getElementById('number-reducer').addEventListener('click', function(event) {
+    if (currentNumber === 1) {
+      return;
+    }
+    currentNumber--;
+    document.getElementById('number').innerHTML = currentNumber;
+  });
+
+  document.getElementById('ok').addEventListener('click', function() {
+    var name = document.getElementById('name').value.trim();
+    if (!name) {
+      return alert('请输入名字');
+    }
+
+
+    var http = new XMLHttpRequest();
+    var url = "http://www.chenfangwei.xyz/api/fill-in";
+    http.open("POST", url, true);
+
+
+    http.setRequestHeader("Content-type", "application/json");
+
+    http.onreadystatechange = function() {//Call a function when the state changes.
+      if( http.readyState == 4 ) {
+        if (http.status == 201) {
+          window.location.href = './thank.html'
+        } else {
+          alert('错误了');
+        }
+      }
+    }
+
+    var data = JSON.stringify({name: name, number: currentNumber});
+    http.send(data);
+  });
+};
+
+
 // window.onload = function() {
 
 //   // var audio = new Audio('http://www.chenfangwei.xyz/bgm.mp3');
