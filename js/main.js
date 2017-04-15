2a8fef06201601463b210f1af510b7aa5ff381c7
fwchen <crystal.tyan@gmail.com>
2017-04-15 23:47:26 +0800

    initial commit

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
 
 
-window.onload = function() {
+// window.onload = function() {
 
-  // var audio = new Audio('http://www.chenfangwei.xyz/bgm.mp3');
-  // audio.play();
+//   // var audio = new Audio('http://www.chenfangwei.xyz/bgm.mp3');
+//   // audio.play();
 
-  var shakeMusic = document.getElementById("shakeMusic");
-  shakeMusic.load();
-  // shakeMusic.play();
-  // window.addEventListener('shake', shakeEventDidOccur, false);
+//   var shakeMusic = document.getElementById("shakeMusic");
+//   shakeMusic.load();
+//   // shakeMusic.play();
+//   // window.addEventListener('shake', shakeEventDidOccur, false);
 
-  function shakeEventDidOccur(obj) {
-    shakeMusic.play();
-  }
-  shakeMusic.addEventListener('play', function() {
+//   function shakeEventDidOccur(obj) {
+//     shakeMusic.play();
+//   }
+//   shakeMusic.addEventListener('play', function() {
 
-    // 当 audio 能够播放后, 移除这个事件
-    window.removeEventListener('touchstart', shakeEventDidOccur, false);
-  }, false);
+//     // 当 audio 能够播放后, 移除这个事件
+//     window.removeEventListener('touchstart', shakeEventDidOccur, false);
+//   }, false);
 
-  window.addEventListener('touchstart', shakeEventDidOccur, false);
+//   window.addEventListener('touchstart', shakeEventDidOccur, false);
 
 
-};
+// };
