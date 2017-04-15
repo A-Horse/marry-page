0d3d8456e9ab2b78dcdf58db732f63b838dbc295
fwchen <crystal.tyan@gmail.com>
2017-04-15 23:35:18 +0800

    initial commit

--- js/main.js
+++ js/main.js
@@ -2,7 +2,11 @@
 
 window.onload = function() {
 
-  var audio = new Audio('http://www.chenfangwei.xyz/bgm.mp3');
-  audio.play();
+  // var audio = new Audio('http://www.chenfangwei.xyz/bgm.mp3');
+  // audio.play();
+
+  var shakeMusic = document.getElementById("shakeMusic");
+  shakeMusic.load();
+  shakeMusic.play();
 
 };
