1012f137a56da264bd2cce14db0ef4324f999331
fwchen <crystal.tyan@gmail.com>
2017-04-15 23:42:33 +0800

    initial commit

--- js/main.js
+++ js/main.js
@@ -7,11 +7,19 @@ window.onload = function() {
 
   var shakeMusic = document.getElementById("shakeMusic");
   shakeMusic.load();
-  shakeMusic.play();
-  window.addEventListener('shake', shakeEventDidOccur, false);
+  // shakeMusic.play();
+  // window.addEventListener('shake', shakeEventDidOccur, false);
 
   function shakeEventDidOccur(obj) {
     shakeMusic.play();
   }
+  shakeMusic.addEventListener('play', function() {
+
+    // 当 audio 能够播放后, 移除这个事件
+    window.removeEventListener('touchstart', shakeEventDidOccur, false);
+  }, false);
+
+  window.addEventListener('touchstart', shakeEventDidOccur, false);
+
 
 };
